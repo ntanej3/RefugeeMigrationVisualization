f3500abc75d94ed269afb7038011ad6e96303677
Neha Taneja <ntanej3@illinois.edu>
2018-09-08 21:26:10 -0700

    Added KMZ for cross platform support

--- GoogleEarthVisualisation.R
+++ GoogleEarthVisualisation.R
@@ -1,52 +1,34 @@
+"
+title: Refugee population migration analysis
+author: Neha Taneja
 
-'''
+This document is used to analyze the refugee population migration for the year 2015.
 
-Author: Neha Taneja
 Data source: https://en.m.wikipedia.org/wiki/List_of_countries_by_refugee_population#By_country_of_origin
-
-'''
-
-#Load the data of LatLon, a dataframe with Country.Name, CIA.Codes, Latitude, Longitude
-load("~/Documents/hw8/hw8.rda")
+"
 
 install.packages(c("XML", "RCurl"))
-
 library(XML)
 library(RCurl)
 
-## XML library tested and resulted document is saved as test_xml
-doc = newXMLDoc()
-parent = newXMLNode("parent", doc = doc)
-child = newXMLNode("child", "leaf node", parent = parent)
-
-saveXML(doc, indent = TRUE, file = "~/Documents/hw8/test_xml.xml")
-
-## RCurl test
-
-text = getURL("http://ptrckprry.com/course/ssd/data/positive-words.txt")
+#Load the data of LatLon, a dataframe with Country.Name, CIA.Codes, Latitude, Longitude
+load("LatLon.rda")
 
 # url from where list of countries by refugee population has to be fetched.
-url = "https://en.m.wikipedia.org/wiki/List_of_countries_by_refugee_population"
-
-
-listOfCountriesByRefugeePopulationHtmlPage = getURL(url = url)
-
+listOfCountriesByRefugeePopulationHtmlPage = getURL(url = "https://en.m.wikipedia.org/wiki/List_of_countries_by_refugee_population")
 
 # Working on the table UNHCR registered refugees by country/territory of asylum between mid-2015 and 2007
-
-refugee  = readHTMLTable(listOfCountriesByRefugeePopulationHtmlPage)[[1]]
+refugee  = readHTMLTable(listOfCountriesByRefugeePopulationHtmlPage)[[2]]
 
 # Replaced the appropriate column names
-refugee = refugee[, c(1, 2, 3)]
+refugee = refugee[-1, c(1, 2, 3)]
 
-names(refugee[, c(1, 2, 3)]) <-
-    c("Country.Name", "refugees_per_1000", "refugee_2015")
+colnames(refugee) <- c("Country.Name", "refugees_per_1000", "refugee_2015")
 
 # Created a dataframe called AllData by merging Country.Name of refugee table with LatLon table
 refugee[, "Country.Name"] = toupper(refugee[, "Country.Name"])
 
-AllData = merge(x = refugee, y = LatLon, by = "Country.Name")
-
+allData = merge(x = refugee, y = LatLon, by = "Country.Name")
 
 # Missing values are replaced with medians in order to clean the data
 replaceMissingByMedian = function(col) {
@@ -58,7 +40,7 @@ replaceMissingByMedian = function(col) {
     return(col)
 }
 
-AllData = apply(AllData, 2, replaceMissingByMedian)
+allData = apply(allData, 2, replaceMissingByMedian)
 
 
 # function to make the base document with the name Refugee_Distribution
@@ -104,17 +86,13 @@ addPlacemark = function(lat,
     
     newXMLNode("Point", newXMLNode("coordinates", paste(lat, lon, sep = ",")), parent = pm)
     
-    ### You need to fill in the code for making the Point node above,
-    ### including coordinates. The line below won't work until you've run
-    ### the code for the next section to set up the styles.
-    
     if (style)
         newXMLNode("styleUrl",
                    paste("#YOR", ref1000Cut, "-", refCut, sep = ''),
                    parent = pm)
 }
 
-# Created kmlDoc
+# Create kmlDoc
 kmlDoc = makeBaseDocument()
 
 document = xmlRoot(kmlDoc)["Document"]
@@ -123,25 +101,16 @@ addPlacemarks = function(row) {
     addPlacemark(row[5], row[6], row[4], row[1], row[3], row[2], document, 1, 1)
 }
 
-apply(AllData, 1, addPlacemarks)
+apply(allData, 1, addPlacemarks)
 
-# saved the kmlDoc as Part2 kml to be opened via Google Earth
-saveXML(kmlDoc, "~/Documents/hw8/Part2.kml")
+saveXML(kmlDoc, "refugee_population.kml")
 
-### Part III.  Add Style to your KML
-### Now you are going to make the visualizatiion a bit fancier. To be more specific, instead of pushpins, we
-### want different circle labels for countris with size refugee population and the color representing
-### the refugees rate.
-### Pretty much all the code is given to you below to create style elements.
-### Here, you just need to figure out what it all does.
-
-### Start fresh with a new KML document, by calling makeBaseDocument()
-
+# Refugee population visualization with visible scale of migratrion.
 doc2 = makeBaseDocument()
 
-ref1000Cut = cut(as.numeric(AllData[, "refugees_per_1000"]), breaks = c(0, 10, 25, 50, 75, 225))
+ref1000Cut = cut(as.numeric(allData[, "refugees_per_1000"]), breaks = c(0, 10, 25, 50, 75, 225))
 ref1000Cut = as.numeric(ref1000Cut)
-refCut = cut(as.numeric(AllData[, "refugee_2015"]), breaks = 5)
+refCut = cut(as.numeric(allData[, "refugee_2015"]), breaks = 5)
 refCut = as.numeric(refCut)
 
 
@@ -178,9 +147,13 @@ for (k in 1:5) {
 }
 
 
-for (rowIndex in 1:nrow(AllData)) {
-    row = AllData[rowIndex, ]
+for (rowIndex in 1:nrow(allData)) {
+    row = allData[rowIndex, ]
     addPlacemark(row[5], row[6], row[4], row[1], row[3], row[2], DocNode, ref1000Cut[rowIndex], refCut[rowIndex], style = TRUE)
 }
 
-saveXML(doc2, "~/Documents/hw8/Part3.kml")
+saveXML(doc2, "scale_of_migration_of_refugee_population.kml")
+
+zip(zipfile = "scale_of_migration_of_refugee_population.kmz", files = c("scale_of_migration_of_refugee_population.kml", "color_label_circle"))
+
+file.remove("scale_of_migration_of_refugee_population.kml")
